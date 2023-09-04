1e40f2f7b7e1d5033b49abf77a25279f4d4b9b08
ThomasKPH14 <Kropfitsch14@Outlook.fr>
2023-09-05 00:56:26 +0200

    Last update

--- src/containers/Form/index.test.js
+++ src/containers/Form/index.test.js
@@ -22,7 +22,7 @@ describe("When Events is created", () => {
         })
       );
       await screen.findByText("En cours");
-      await screen.findByText("Envoyer");
+      await screen.findByTestId("button-test-id");
       expect(onSuccess).toHaveBeenCalled();
     });
   });
